4e0faaded2c719d10c7eeb074986a3bf4a3f7a7d
zorin375 <vitormanoelbasilio357@gmail.com>
2022-12-17 00:07:22 -0300

    quiz java

---
+++ Qiz/src/br/com/quiz/VitorOzorio/logica/Logica.java
@@ -0,0 +1,20 @@
+package br.com.quiz.VitorOzorio.logica;
+
+public class Logica {
+
+   private int contador ;
+
+    public Logica(int contador) {
+        this.contador = contador;
+    }
+
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+
+}
---
+++ Qiz/src/br/com/quiz/VitorOzorio/quiz.java
@@ -0,0 +1,204 @@
+package br.com.quiz.VitorOzorio;
+import br.com.quiz.VitorOzorio.logica.Logica;
+import java.util.Scanner;
+
+
+public class quiz {
+    public static void main(String[] args) {
+        Scanner texto = new Scanner(System.in);
+        int opcao ;
+        int contador = 1;
+        Logica Adicionar = new Logica(1);
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|vamos fazer um teste !!                                    |");
+        System.out.println("|voçê tera que concluir um desafio relacionado a informatica|");
+        System.out.println("|vamos começar                                              |");
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|o que é um processador ?                                   |");
+        System.out.println("|opções                                                     |");
+        System.out.println("|*1 unidade central de processamento                        |");
+        System.out.println("|*2 unidade de memoria                                      |");
+        System.out.println("|*3 monitor                                                 |");
+        System.out.println("|escolha uma opção                                          |");
+        System.out.println("+-----------------------------------------------------------+");
+        opcao = texto.nextInt();
+        System.out.println(opcao);
+        System.out.println(" OK !! VAMOS CONTINUAR");
+
+        if (opcao == 1) {
+
+           Adicionar.setContador(contador++);
+
+        }
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|o que é O html ?                                           |");
+        System.out.println("|opções                                                     |");
+        System.out.println("|*1 ferramenta de trabalho                                  |");
+        System.out.println("|*2 memoria variavel                                        |");
+        System.out.println("|*3 linguagem de marcação                                   |");
+        System.out.println("|escolha uma opção                                          |");
+        System.out.println("+-----------------------------------------------------------+");
+        opcao = texto.nextInt();
+        System.out.println(opcao);
+        System.out.println(" OK !! VAMOS CONTINUAR");
+
+        if (opcao == 3) {
+
+            Adicionar.setContador(contador++);
+
+        }
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|o que é O teclado ?                                        |");
+        System.out.println("|opções                                                     |");
+        System.out.println("|*1 gabinete                                                |");
+        System.out.println("|*2 despositivo de entrada de dados                         |");
+        System.out.println("|*3 cabo de tomada                                          |");
+        System.out.println("|escolha uma opção                                          |");
+        System.out.println("+-----------------------------------------------------------+");
+        opcao = texto.nextInt();
+        System.out.println(opcao);
+        System.out.println(" OK !! VAMOS CONTINUAR");
+
+        if (opcao == 2) {
+
+            Adicionar.setContador(contador++);
+
+        }
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|o que é O protocolo tcp ?                                  |");
+        System.out.println("|opções                                                     |");
+        System.out.println("|*1 um protocolo de transferencia de dados                  |");
+        System.out.println("|*2 uma api                                                 |");
+        System.out.println("|*3 uma marca de processador                                |");
+        System.out.println("|escolha uma opção                                          |");
+        System.out.println("+-----------------------------------------------------------+");
+        opcao = texto.nextInt();
+        System.out.println(opcao);
+        System.out.println(" OK !! VAMOS CONTINUAR");
+
+        if (opcao == 1) {
+
+            Adicionar.setContador(contador++);
+
+        }
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|o que é um hd ?                                            |");
+        System.out.println("|opções                                                     |");
+        System.out.println("|*1 um despositivo de armazenamento de dados                |");
+        System.out.println("|*2 uma linguagem de programação                            |");
+        System.out.println("|*3 uma marca de caixa de som                               |");
+        System.out.println("|escolha uma opção                                          |");
+        System.out.println("+-----------------------------------------------------------+");
+        opcao = texto.nextInt();
+        System.out.println(opcao);
+        System.out.println(" OK !! VAMOS CONTINUAR");
+
+        if (opcao == 1) {
+
+            Adicionar.setContador(contador++);
+
+        }
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|o que é um monitor ?                                       |");
+        System.out.println("|opções                                                     |");
+        System.out.println("|*1 uma marca de teclado                                    |");
+        System.out.println("|*2 uma linguagem de programação                            |");
+        System.out.println("|*3 um despositivo de saida de dados                        |");
+        System.out.println("|escolha uma opção                                          |");
+        System.out.println("+-----------------------------------------------------------+");
+        opcao = texto.nextInt();
+        System.out.println(opcao);
+        System.out.println(" OK !! VAMOS CONTINUAR");
+
+        if (opcao == 3) {
+
+            Adicionar.setContador(contador++);
+
+        }
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|o que é uma placa de video ?                               |");
+        System.out.println("|opções                                                     |");
+        System.out.println("|*1 uma marca de pendrive                                   |");
+        System.out.println("|*2 um componente responsavel pela renderização             |");
+        System.out.println("|*3 um despositivo de modelagem de dados                    |");
+        System.out.println("|escolha uma opção                                          |");
+        System.out.println("+-----------------------------------------------------------+");
+        opcao = texto.nextInt();
+        System.out.println(opcao);
+        System.out.println(" OK !! VAMOS CONTINUAR");
+
+        if (opcao == 2) {
+
+            Adicionar.setContador(contador++);
+
+        }
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|o que é uma fonte ?                                        |");
+        System.out.println("|opções                                                     |");
+        System.out.println("|*1 uma marca de monitores                                  |");
+        System.out.println("|*2 um componente responsavel pela memoria                  |");
+        System.out.println("|*3 um componente que distribui a energia                   |");
+        System.out.println("|escolha uma opção                                          |");
+        System.out.println("+-----------------------------------------------------------+");
+        opcao = texto.nextInt();
+        System.out.println(opcao);
+        System.out.println(" OK !! VAMOS CONTINUAR");
+
+        if (opcao == 3) {
+
+            Adicionar.setContador(contador++);
+
+        }
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|o que é git ?                                              |");
+        System.out.println("|opções                                                     |");
+        System.out.println("|*1 um versionador de codigo                                |");
+        System.out.println("|*2 um protocolo tcp                                        |");
+        System.out.println("|*3 um componente que distribui a memoria                   |");
+        System.out.println("|escolha uma opção                                          |");
+        System.out.println("+-----------------------------------------------------------+");
+        opcao = texto.nextInt();
+        System.out.println(opcao);
+        System.out.println(" OK !! VAMOS CONTINUAR");
+
+        if (opcao == 1) {
+
+            Adicionar.setContador(contador++);
+
+        }
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|o que é um sgbd   ?                                        |");
+        System.out.println("|opções                                                     |");
+        System.out.println("|*1 um ssd portatil                                         |");
+        System.out.println("|*2 um gerenciador de banco de dados                        |");
+        System.out.println("|*3 um componente que da video                              |");
+        System.out.println("|escolha uma opção                                          |");
+        System.out.println("+-----------------------------------------------------------+");
+        opcao = texto.nextInt();
+        System.out.println(opcao);
+        System.out.println(" OK !! VAMOS CONTINUAR");
+
+        if (opcao == 2) {
+
+            Adicionar.setContador(contador++);
+
+        }
+
+        System.out.println("+-----------------------------------------------------------+");
+        System.out.println("|                     muito bem !!                          |");
+        System.out.println("| Voce acertou :"+Adicionar.getContador()+" de 10 perguntas                          |");
+        System.out.println("+-----------------------------------------------------------+");
+
+    }
+}
